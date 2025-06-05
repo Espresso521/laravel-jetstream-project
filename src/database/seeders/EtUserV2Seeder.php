7a9d0e756fd66f0d44a0666944f7af4b5416312b
Espresso521 <kotaku20220424@gmail.com>
2025-06-05 15:17:32 +0900

    Add Seeder for EtUserV2

---
+++ src/database/seeders/EtUserV2Seeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\EtUserV2;
+
+class EtUserV2Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        EtUserV2::create([
+            'CHARGEID' => 'C001',
+            'CHARGE' => 'HUZE',
+            'USERNAME' => 'zhangsan',
+            'PASS' => 'pass123',
+            'DISPTYPE' => '1',
+            'AUTHORITY' => '7',
+            'AUTHORITYSUB' => '0',
+            'JUDGEMENTAREA' => 'A',
+            'SUPERUSER' => '1',
+            'UPDATETIME' => now()->format('YmdHis'),
+            'UPDATEUSER' => 'admin',
+        ]);
+
+        EtUserV2::create([
+            'CHARGEID' => 'C002',
+            'CHARGE' => '金子',
+            'USERNAME' => 'かねこ',
+            'PASS' => '321abc',
+            'DISPTYPE' => '2',
+            'AUTHORITY' => '8',
+            'AUTHORITYSUB' => '1',
+            'JUDGEMENTAREA' => 'B',
+            'SUPERUSER' => '1',
+            'UPDATETIME' => now()->format('YmdHis'),
+            'UPDATEUSER' => 'test',
+        ]);
+    }
+}
